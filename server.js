561fc41f406280b056a36ceacce22e55563895f5
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2025-12-12 07:41:26 +0000

    version 0.0.3

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const cors = require("cors")
 const express = require("express")
 
 const { v4: uuidv4 } = require("uuid")
-const { WebSocketServer } = require("ws")
 
 const app = express()
 const server = http.createServer(app)
-const io = new WebSocketServer({ noServer: true })
 
 const router = {
     media: express.Router(),
     accounts: express.Router(),
     events: express.Router(),
     communities: express.Router(),
-    earth: express.Router()
+    inbox: express.Router(),
+    missions: express.Router()
 }
 
 const storage = {
@@ -25,7 +24,10 @@ const storage = {
             fs.writeFileSync("./storage/accounts.json", JSON.stringify(Object.fromEntries([
                 [uuidv4().split("-")[4], {
                     username: "zavier09",
-                    token: [uuidv4(), Date.now() + (24 * 60 * 60 * 1000)]
+                    token: [uuidv4(), Date.now() + (24 * 60 * 60 * 1000)],
+                    missions: {},
+                    inbox: {},
+                    statistics: { hearts: 0 }
                 }]
             ])))
         }
@@ -45,16 +47,7 @@ const storage = {
             fs.writeFileSync("./storage/communities.json", JSON.stringify(Object.fromEntries([])))
         }
         return JSON.parse(fs.readFileSync("./storage/communities.json"))
-    })(),
-    earth: (function(){
-        if (!fs.existsSync("./storage/earth.json")) {
-            fs.writeFileSync("./storage/earth.json", JSON.stringify(Object.fromEntries([])))
-        }
-        return JSON.parse(fs.readFileSync("./storage/earth.json"))
-    })(),
-    temporary: {
-
-    }
+    })()
 }
 
 app.use(cors())
@@ -88,14 +81,15 @@ app.use((request, response, next) => {
 })
 
 app.get("/", (request, response) => {
-    response.sendFile(storage.media["aG9t"])
+    response.sendFile(storage.media["aW5k"])
 })
 
 app.use("/media", router.media)
 app.use("/accounts", router.accounts)
 app.use("/events", router.events)
 app.use("/communities", router.communities)
-app.use("/earth", router.earth)
+app.use("/inbox", router.inbox)
+app.use("/missions", router.missions)
 
 router.media.get("/:id", (request, response) => {
     if (storage.media[request.params.id]) {
@@ -230,32 +224,74 @@ router.events.post("/:id/join", (request, response) => {
 })
 
 router.communities.get("/search", (request, response) => {
-
+    if (request.query.query) {
+        response.json(Object.fromEntries(Object.entries(events).filter((event) => [event[1].name, event[1].tags, event[1].description].flat().join("").replace(/\W/g, "").toLowerCase().includes(request.query.query.replace(/\W/g, "").toLowerCase()))))
+    }
 })
 router.communities.post("/create", (request, response) => {
-
+    id = uuidv4().split("-")[1]
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    storage.communities[id] = {
+        name: request.body.name,
+        description: request.body.description,
+        owner: account[0],
+        members: [account[0]],
+        tags: request.body.tags,
+        posts: {}
+    }
+    response.json(storage.communities[id])
 })
 router.communities.get("/:communityId", (request, response) => {
-
-})
-router.communities.get("/:communityId/post/:postId", (request, response) => {
-    
-})
-
-router.earth.get("/", (request, response) => {
-
+    if (storage.communities[request.params.communityId]) {
+        response.json(storage.communities[request.params.communityId])
+    }
 })
 
-io.on("connection", (socket, request) => {
-
+router.inbox.get("/", (request, response) => {
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    response.json(Object.fromEntries(Object.entries(account[1].inbox).filter((mail) => (Date.now() - mail[1].timestamp) < (10 * 24 * 60 * 60 * 1000))))
+})
+router.inbox.post("/send", (request, response) => {
+    id = uuidv4().split("-")[2]
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    account[1].inbox[id] = {
+        title: request.body.title,
+        to: request.body.to,
+        from: account[0],
+        content: request.body.content,
+        timestamp: Date.now()
+    }
+    response.json(account[1].inbox[id])
 })
 
-server.on("upgrade", (request, socket, head) => {
-    io.handleUpgrade(request, socket, head, (socket, request) => {
-        io.emit("connection", socket, request)
-    })
+router.missions.get("/", (request, response) => {
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    console.log(account)
+    response.json(account[1].missions)
+})
+router.missions.post("/verify/:id", (request, response) => {
+
+})
+router.missions.post("/accept", (request, response) => {
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    console.log(request.body.mailId, Object.entries(account[1].inbox).filter((mail) => mail[1].to === account[0]))
 })
 
 server.listen(5000, () => {
+    id = uuidv4().split("-")[2]
+    storage.accounts["20bc3370e408"].inbox[id] = {
+        title: "Are you Ready?",
+        from: "AMOR HQ",
+        content: "Dear Agent Zavier,<br><br>Welcome to Project Amor.<br>We are an international group created for one purpose: to save the world through love.<br><br>Your first mission is to spend time with a loved one.<br>Are you up for the job?<br><br>This message would be auto deleted in 10 days.<br><br>Yours Sincerely,<br>President of Project Amor",
+        timestamp: Date.now(),
+        mission: Object.fromEntries([
+            [uuidv4().split("-")[3], { name: "Spend time with a loved one", timestamp: Date.now() }]
+        ])
+    }
     console.log(storage)
 })
