bdf628058f308668f7de32b67e1f2e30516625e9
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2025-12-12 12:13:37 +0000

    version 0.0.4

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 const express = require("express")
 
 const { v4: uuidv4 } = require("uuid")
+const { setHeapSnapshotNearHeapLimit } = require("v8")
 
 const app = express()
 const server = http.createServer(app)
@@ -13,7 +14,7 @@ const router = {
     media: express.Router(),
     accounts: express.Router(),
     events: express.Router(),
-    communities: express.Router(),
+    community: express.Router(),
     inbox: express.Router(),
     missions: express.Router()
 }
@@ -28,13 +29,20 @@ const storage = {
                     missions: {},
                     inbox: {},
                     statistics: { hearts: 0 }
+                }],
+                [uuidv4().split("-")[4], {
+                    username: "jeyrome92",
+                    token: [uuidv4(), Date.now() + (24 * 60 * 60 * 1000)],
+                    missions: {},
+                    inbox: {},
+                    statistics: { hearts: 0 }
                 }]
             ])))
         }
         return JSON.parse(fs.readFileSync("./storage/accounts.json"))
     })(),
     media: (function(){
-        return Object.fromEntries(["/storage/media"].map((path, index) => fs.readdirSync(__dirname + "/" + path).filter((filename) => filename.includes(".")).map((filename) => ([btoa(filename).slice(0, (2 ** (index + 2))), __dirname + path + "/" + filename]))).flat())
+        return Object.fromEntries(["/storage/media", "/storage/media/earth"].map((path, index) => fs.readdirSync(__dirname + "/" + path).filter((filename) => filename.includes(".")).map((filename) => ([btoa(filename).slice(0, (2 ** (index + 2))), __dirname + path + "/" + filename]))).flat())
     })(),
     events: (function(){
         if (!fs.existsSync("./storage/events.json")) {
@@ -42,11 +50,19 @@ const storage = {
         }
         return JSON.parse(fs.readFileSync("./storage/events.json"))
     })(),
-    communities: (function(){
-        if (!fs.existsSync("./storage/communities.json")) {
-            fs.writeFileSync("./storage/communities.json", JSON.stringify(Object.fromEntries([])))
+    community: (function(){
+        if (!fs.existsSync("./storage/community.json")) {
+            fs.writeFileSync("./storage/community.json", JSON.stringify([]))
         }
-        return JSON.parse(fs.readFileSync("./storage/communities.json"))
+        return JSON.parse(fs.readFileSync("./storage/community.json"))
+    })(),
+    admin: (function(){
+        if (!fs.existsSync("./storage/admin.json")) {
+            fs.writeFileSync("./storage/admin.json", JSON.stringify({
+                queue: []
+            }))
+        }
+        return JSON.parse(fs.readFileSync("./storage/admin.json"))
     })()
 }
 
@@ -87,7 +103,7 @@ app.get("/", (request, response) => {
 app.use("/media", router.media)
 app.use("/accounts", router.accounts)
 app.use("/events", router.events)
-app.use("/communities", router.communities)
+app.use("/community", router.community)
 app.use("/inbox", router.inbox)
 app.use("/missions", router.missions)
 
@@ -103,7 +119,7 @@ router.accounts.post("/authorize", (request, response) => {
         authorization = JSON.parse(atob(request.headers["authorization"]))
         if (authorization.token) {
             if (Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token).length > 0) {
-                response.json(Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0][1])
+                response.json(Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0])
             } else {
                 response.json({ error: [100, "Invalid token."] })
             }
@@ -113,7 +129,7 @@ router.accounts.post("/authorize", (request, response) => {
                 // Verify two-factor
                 storage.accounts[account[0]].token = [uuidv4(), Date.now() + (24 * 60 * 60 * 1000)]
                 fs.writeFileSync("./storage/accounts.json", JSON.stringify(storage.accounts))
-                response.json(storage.accounts[account[0]])
+                response.json([account[0], storage.accounts[account[0]]])
             } else {
                 response.json({ error: [101, "Invalid username."] })
             }
@@ -208,6 +224,7 @@ router.events.delete("/:id", (request, response) => {
         account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
         if (account[0] === storage.events[request.params.id].owner) {
             delete storage.events[request.params.id]
+            fs.writeFileSync("./storage/event.json", JSON.stringify(storage.events))
             response.json({})
         }
     }
@@ -218,34 +235,26 @@ router.events.post("/:id/join", (request, response) => {
         account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
         if (!storage.events[request.params.id].participants.includes(account[0])) {
             storage.events[request.params.id].participants.push(account[0])
+            fs.writeFileSync("./storage/event.json", JSON.stringify(storage.events))
             response.json({})
         }
     }
 })
 
-router.communities.get("/search", (request, response) => {
-    if (request.query.query) {
-        response.json(Object.fromEntries(Object.entries(events).filter((event) => [event[1].name, event[1].tags, event[1].description].flat().join("").replace(/\W/g, "").toLowerCase().includes(request.query.query.replace(/\W/g, "").toLowerCase()))))
-    }
+router.community.get("/", (request, response) => {
+    response.json(storage.community.sort(function(a, b){ return b.timestamp - a.timestamp }).slice(0, 50))
 })
-router.communities.post("/create", (request, response) => {
-    id = uuidv4().split("-")[1]
+router.community.post("/post", (request, response) => {
+    console.log(request)
     authorization = JSON.parse(atob(request.headers["authorization"]))
     account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
-    storage.communities[id] = {
-        name: request.body.name,
-        description: request.body.description,
-        owner: account[0],
-        members: [account[0]],
-        tags: request.body.tags,
-        posts: {}
-    }
-    response.json(storage.communities[id])
-})
-router.communities.get("/:communityId", (request, response) => {
-    if (storage.communities[request.params.communityId]) {
-        response.json(storage.communities[request.params.communityId])
-    }
+    storage.community.push({
+        from: [account[0], account[1].username],
+        content: request.body.content,
+        timestamp: Date.now()
+    })
+    fs.writeFileSync("./storage/community.json", JSON.stringify(storage.community))
+    response.json(storage.community[storage.community.length-1])
 })
 
 router.inbox.get("/", (request, response) => {
@@ -274,23 +283,36 @@ router.missions.get("/", (request, response) => {
     response.json(account[1].missions)
 })
 router.missions.post("/verify/:id", (request, response) => {
+    authorization = JSON.parse(atob(request.headers["authorization"]))
+    account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
+    if (account[1].missions[request.params.id]) {
 
+    }
 })
 router.missions.post("/accept", (request, response) => {
     authorization = JSON.parse(atob(request.headers["authorization"]))
     account = Object.entries(storage.accounts).filter((account) => account[1].token[0] === authorization.token)[0]
-    console.log(request.body.mailId, Object.entries(account[1].inbox).filter((mail) => mail[1].to === account[0]))
+    if (account[1].inbox[request.body.mailId]) {
+        if (account[1].inbox[request.body.mailId].missions) {
+            Object.entries(account[1].inbox[request.body.mailId].missions).forEach((mission) => {
+                account[1].missions[mission[0]] = mission[1]
+            })
+            delete account[1].inbox[request.body.mailId]
+            response.json(account[1].missions)
+        }
+    }
+    // console.log(request.body.mailId, Object.entries(account[1].inbox).filter((mail) => mail[1].to === account[0]))
 })
 
 server.listen(5000, () => {
     id = uuidv4().split("-")[2]
-    storage.accounts["20bc3370e408"].inbox[id] = {
+    storage.accounts["c2da2346c2c0"].inbox[id] = {
         title: "Are you Ready?",
         from: "AMOR HQ",
-        content: "Dear Agent Zavier,<br><br>Welcome to Project Amor.<br>We are an international group created for one purpose: to save the world through love.<br><br>Your first mission is to spend time with a loved one.<br>Are you up for the job?<br><br>This message would be auto deleted in 10 days.<br><br>Yours Sincerely,<br>President of Project Amor",
+        content: "Dear Agent Jeyrome,<br><br>Welcome to Project Amor.<br>We are an international group created for one purpose: to save the world through love.<br><br>Your first mission is to spend time with a loved one.<br>Are you up for the job?<br><br>This message would be auto deleted in 10 days.<br><br>Yours Sincerely,<br>President of Project Amor",
         timestamp: Date.now(),
-        mission: Object.fromEntries([
-            [uuidv4().split("-")[3], { name: "Spend time with a loved one", timestamp: Date.now() }]
+        missions: Object.fromEntries([
+            [uuidv4().split("-")[3], { title: "Spend time with a loved one", timestamp: Date.now() }]
         ])
     }
     console.log(storage)
